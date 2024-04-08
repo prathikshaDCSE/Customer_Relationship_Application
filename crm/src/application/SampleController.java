7400114169bb36dc3cf3e17c70edeab6e6051829
prathikshaDCSE <126931618+prathikshaDCSE@users.noreply.github.com>
2024-04-08 14:10:40 +0530

    Add files via upload

---
+++ crm/src/application/SampleController.java
@@ -0,0 +1,20 @@
+package com.example.customer_relationship;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class SampleController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
---
+++ crm/src/module-info.java
@@ -0,0 +1,5 @@
+module crm {
+	requires javafx.controls;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
